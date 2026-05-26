2e994c6e09dce00ea86a27f0481f5a9b1b504b68
Tony-creator768 <157574456+Tony-creator768@users.noreply.github.com>
2026-05-26 16:03:25 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Paste your Google Apps Script Web App URL below.
+// This package keeps your current URL. If the site does not load live data, redeploy Code.gs and paste the new /exec URL here.
+window.CATALOGUE_CONFIG = {
+  DATA_URL: "https://script.google.com/a/macros/georgetown.edu/s/AKfycbwJb8OgmkZg_gOzthKfjbyFP7oYieuqaM9-tGwOHbkKLikI9iPvV_2zTZguLEEtY2AB/exec",
+  APPROVED_ONLY: true,
+  SHOW_INTERNAL_NOTES: false,
+  CURRENCY: "QAR"
+};
