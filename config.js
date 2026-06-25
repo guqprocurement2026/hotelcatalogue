31992e8c7f9ee72b0043b6845a515ddb0676d13b
guqfinance <157574456+Tony-creator768@users.noreply.github.com>
2026-06-25 13:25:36 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Paste your Google Apps Script Web App URL below.
-// This package keeps your current URL. If the site does not load live data, redeploy Code.gs and paste the new /exec URL here.
-window.CATALOGUE_CONFIG = {
-  DATA_URL: "https://script.google.com/macros/s/AKfycbyHIX_yzL1Dugjj9oa8gCRPysS5uftJl2I0XbUCEnA0ZEfJhs5uoRWPE8PAkfucztoI/exec",
-  APPROVED_ONLY: true,
-  SHOW_INTERNAL_NOTES: false,
-  CURRENCY: "QAR"
-};
